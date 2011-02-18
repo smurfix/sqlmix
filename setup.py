bc5b79b37486be9c3d9e6eae9e0dd9e3789d73db
Matthias Urlichs <matthias@urlichs.de>
2011-02-18 09:05:54 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""Setup script for smurf's Python database module"""
+
+from distutils.core import setup
+
+description = "Simple syntax for any database"
+long_description = \
+"""
+This module contains database access methods which are very easy
+to use and work with any Python database module without touching
+parameterized SQL syntax.
+
+"""
+
+setup (name = "dbmix",
+       version = "0.9",
+       description = description,
+       long_description = long_description,
+       author = "Matthias Urlichs",
+       author_email = "smurf@smurf.noris.de",
+       url = "http://smurf.noris.de/code/",
+       license = 'Python',
+       platforms = ['POSIX'],
+       packages = ['dbmix'],
+      )
